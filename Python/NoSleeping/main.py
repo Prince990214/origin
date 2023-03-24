f06c531d021434c2b60508d337cc536488f839a5
Prince990214 <zi.wang@vitesco.com>
2023-03-24 10:36:50 +0800

    230324: Add prevent sleep function by PyautoGUI

---
+++ Python/NoSleeping/main.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+while 1:
+    #pyautogui.FAILSAFE = False
+    pyautogui.moveTo(544, 300)
+    pyautogui.click()
+    time.sleep(10)
+    pyautogui.moveTo(644, 144)
+    time.sleep(10)
+
+
+
